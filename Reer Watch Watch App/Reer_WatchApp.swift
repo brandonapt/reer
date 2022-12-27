228ad692bebf05cc5bd29e4f5ef23b56c9d8daac
Brandon! <brandoge91@gmail.com>
2022-12-27 10:07:45 -0500

    intial ios commit

---
+++ Reer Watch Watch App/Reer_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reer_WatchApp.swift
+//  Reer Watch Watch App
+//
+//  Created by Brandon Saraceno on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Reer_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ reer/reerApp.swift
@@ -0,0 +1,17 @@
+//
+//  reerApp.swift
+//  reer
+//
+//  Created by Brandon Saraceno on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct reerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
